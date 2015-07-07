b4895209b9a65991a1e5079ed62e4ae1a4c92048
Roy Meissner <roymeissn@googlemail.com>
2015-07-07 13:35:56 +0200

    Resolved shutdown exception

--- app/Global.scala
+++ app/Global.scala
@@ -1,9 +1,14 @@
-import resources.Extractor.cacheIt
+import akka.actor.ActorSelection._
+import akka.actor.PoisonPill
 import play.api.Application
 import play.api.GlobalSettings
 import play.api.Logger
+import play.api.Play.current
+import play.api.libs.concurrent.Execution.Implicits._
 import play.api.mvc.WithFilters
 import play.filters.gzip.GzipFilter
+import play.libs.Akka
+import resources.Extractor.cacheIt
 
 object Global extends WithFilters(new GzipFilter() /*, new CorsFilter()*/ ) with GlobalSettings {
 
@@ -14,5 +19,6 @@ object Global extends WithFilters(new GzipFilter() /*, new CorsFilter()*/ ) with
 
   override def onStop(app: Application) {
     Logger.info("Application shutdown...")
+    Akka.system.actorSelection("*").tell(PoisonPill, null)
   }
 }
