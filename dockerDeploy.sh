16f552727defb1807a6abfde9104fd8de31a4f10
Roy Meissner <roy-meissner@gmx.net>
2015-11-12 09:23:12 +0100

    removed docker execution (now optional) [skip ci]

--- dockerDeploy.sh
+++ dockerDeploy.sh
@@ -5,10 +5,8 @@ activator clean docker:stage
 cp Dockerfile target/docker/
 cp newrelic-java-3.17.0.zip target/docker/newrelic.zip
 
-cd target/docker/
-docker build -t rmeissn/buildings:latest ./
-
-cd ../../../
-docker-compose up -d --no-deps buldingsService
-docker rmi rmeissn/buildings:current
-docker tag rmeissn/buildings:latest rmeissn/buildings:current
+echo "Remember to run Docker-Compose on HTWK-App Server or, for local tests:\n\n"
+echo "cd target/docker/\n"
+echo "docker build -t rmeissn/buildings:latest ./\n"
+echo "docker run -d -p 9000:9000 rmeissn/buildings:latest\n\n"
+echo "Don't forget to delete old docker images/containers!\n"
